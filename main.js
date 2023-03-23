77d16654756d08b297d00873f241de2266701d23
girassol2023 <123131164+girassol2023@users.noreply.github.com>
2023-03-23 18:52:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+canvas =
+document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+function myMouseDown(e) {
+ color = document.getElementById("color").value;
+ widthLine = document.getElementById("widthLine").value;
+ radius = document.getElementById("radius").value;
+ MouseEvent = "mouseDown";
+
+}
